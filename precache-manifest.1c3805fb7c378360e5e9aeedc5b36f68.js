907cac308fc30d57234368898186434003825b29
BKirilov <bkirilov2000@gmail.com>
2020-08-26 12:55:23 +0300

    Updates

---
+++ precache-manifest.1c3805fb7c378360e5e9aeedc5b36f68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f4f371f7a1c30071be5fb608deded0",
+    "url": "/JavaScript-Calculator/index.html"
+  },
+  {
+    "revision": "c3cac1f645802c55ac55",
+    "url": "/JavaScript-Calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4668a3e723959ffae6f",
+    "url": "/JavaScript-Calculator/static/js/2.669eb923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JavaScript-Calculator/static/js/2.669eb923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3cac1f645802c55ac55",
+    "url": "/JavaScript-Calculator/static/js/main.23462114.chunk.js"
+  },
+  {
+    "revision": "28df921d83528441290c",
+    "url": "/JavaScript-Calculator/static/js/runtime-main.1b7f58fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JavaScript-Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
